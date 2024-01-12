cab35096c546c55a723012a4a924f6d715d9371b
Gustavo Godoy <domingues.godoy@hotmail.com.br>
2024-01-11 23:20:44 -0300

    Melhorias na logica, estrutra e estilo

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 var getInput = document.getElementById('input');
 var voceDisseElement = document.getElementById('voceDisse');
 var cebolinhaDisse = document.getElementById('cebolinhaDisse');
+var getClasseTraduzido = document.getElementsByClassName('traduzido');
+
+for(var i = 0; i < getClasseTraduzido.length; i+=1){
+    getClasseTraduzido[i].style.display = 'none'
+}
 
 function traduzir() {
     if(!getInput.value) return;
-    let frase = getInput.value;
-    let traduzido = frase.toLowerCase().replaceAll("r", "l");
-    voceDisseElement.innerHTML = frase;
-    cebolinhaDisse.innerHTML = traduzido;
 
+    if(getInput.value){
+        let frase = getInput.value;
+        let traduzido = frase.toLowerCase().replaceAll("r", "l");
+        voceDisseElement.innerHTML = frase;
+        cebolinhaDisse.innerHTML = traduzido;
+
+        for(var i = 0; i < getClasseTraduzido.length; i+=1) {
+            getClasseTraduzido[i].style.display = 'block';
+        }
+    }
 
     getInput.value = "";
 }
